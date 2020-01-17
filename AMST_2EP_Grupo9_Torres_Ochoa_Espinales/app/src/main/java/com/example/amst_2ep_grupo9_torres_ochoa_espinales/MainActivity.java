bb0864f3deffb603c1f4666715b9304eb5dc41fc
Danilo Torres Intriago <danilo.ti@hotmail.com>
2020-01-17 12:40:34 -0500

    cambios

--- AMST_2EP_Grupo9_Torres_Ochoa_Espinales/app/src/main/java/com/example/amst_2ep_grupo9_torres_ochoa_espinales/MainActivity.java
+++ AMST_2EP_Grupo9_Torres_Ochoa_Espinales/app/src/main/java/com/example/amst_2ep_grupo9_torres_ochoa_espinales/MainActivity.java
@@ -5,17 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,7 +27,6 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             System.out.println(e);
         }
-
         /*
         v.getId("button_buscar");
         R.id.button_buscar;
@@ -38,23 +34,47 @@ public class MainActivity extends AppCompatActivity {
          */
     }
 
-    public void buscarHeroe() throws IOException {
+    public void buscarHeroe(){
         String url_api = "https://superheroapi.com/api/2979915212042299/1";
 
         JSONObject j = new JSONObject();
-        //JsonObjectRequest.Method.GET
-        JsonObjectRequest heroes = new JsonObjectRequest(url_api, j,"", new Response.ErrorListener );
 
-        URL url = new URL("https://superheroapi.com/api/2979915212042299/1");
-        System.out.println("Antes de la coneccion");
-        HttpsURLConnection coneccion = (HttpsURLConnection) url.openConnection();
-        System.out.println("Conectado");
-        coneccion.setRequestMethod("GET");
-        System.out.println("Metodo GET");
-        coneccion.connect();
-        System.out.println("Conect");
-        System.out.println(coneccion.getResponseMessage());
-        System.out.println("Mensaje");
+        /*
+        JsonArrayRequest request_heroes = new JsonArrayRequest(url_api,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        System.out.println(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+        });
+        */
+
+        JsonObjectRequest heroes = new JsonObjectRequest(Request.Method.GET, url_api, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    System.out.println(response.get("response"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //System.out.println(error);
+            }
+        });
+        System.out.println(heroes);
+        System.out.println(j);
+
 
     }
 }
