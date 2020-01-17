0156e18de6efe63956a5108fb5bb89a3ff8deae8
Danilo Torres Intriago <danilo.ti@hotmail.com>
2020-01-17 14:22:38 -0500

    solo falta el grafico

--- AMST_2EP_Grupo9_Torres_Ochoa_Espinales/app/src/main/java/com/example/amst_2ep_grupo9_torres_ochoa_espinales/MainActivity.java
+++ AMST_2EP_Grupo9_Torres_Ochoa_Espinales/app/src/main/java/com/example/amst_2ep_grupo9_torres_ochoa_espinales/MainActivity.java
@@ -2,92 +2,61 @@ package com.example.amst_2ep_grupo9_torres_ochoa_espinales;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
-    View v;
+    RequestQueue lista = null;
+    TextView editText;
+    public static JSONObject busqueda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        try {
-            buscarHeroe();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        /*
-        v.getId("button_buscar");
-        R.id.button_buscar;
-
-         */
+        lista = Volley.newRequestQueue(this);
     }
 
     public void imprimirResponse(JSONObject response) {
-        try {
-            System.out.println(response.get("response"));
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        busqueda = response;
+        Intent i = new Intent(this, Resultado.class);
+        startActivity(i);
     }
 
-    public void buscarHeroe(){
-        String url_api = "https://superheroapi.com/api/2979915212042299/1";
+    public void buscarHeroe(View v){
+        String url_api = "https://superheroapi.com/api/2979915212042299/search/";
 
-        JSONObject j = new JSONObject();
+        editText = findViewById(R.id.editText);
+        String buscar = editText.getText().toString();
+        url_api = String.format("%s%s", url_api, buscar);
 
-        /*
-        JsonArrayRequest request_heroes = new JsonArrayRequest(url_api,
-                new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        System.out.println(response);
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                    }
-        });
-        */
-
-        System.out.println("antes del request");
         JsonObjectRequest heroes = new JsonObjectRequest(Request.Method.GET, url_api, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-                System.out.println("on response");
                 imprimirResponse(response);
-                try {
-                    System.out.println(response.get("response"));
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                System.out.println(response);
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                System.out.println("Error");
-                //System.out.println(error);
+                System.out.println(error);
             }
         });
 
-        //System.out.println(heroes.getTimeoutMs());
-        //System.out.println(heroes.hasHadResponseDelivered());
-
+        lista.add(heroes);
 
     }
 }
--- AMST_2EP_Grupo9_Torres_Ochoa_Espinales/app/src/main/java/com/example/amst_2ep_grupo9_torres_ochoa_espinales/Resultado.java
+++ AMST_2EP_Grupo9_Torres_Ochoa_Espinales/app/src/main/java/com/example/amst_2ep_grupo9_torres_ochoa_espinales/Resultado.java
@@ -2,13 +2,61 @@ package com.example.amst_2ep_grupo9_torres_ochoa_espinales;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
+import java.util.List;
 
 public class Resultado extends AppCompatActivity {
+    JSONObject busqueda = MainActivity.busqueda;
+    private LinearLayout contenedor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+        contenedor = findViewById(R.id.cont_heroes);
+        try {
+            JSONArray l = (JSONArray) busqueda.get("results");
+            for (int i  = 0; i < l.length(); i++) {
+                JSONObject j = (JSONObject) l.get(i);
+                TextView x = new TextView(this);
+                x.setText(j.get("id").toString()+", "+j.get("name").toString());
+                x.callOnClick();
+                contenedor.addView(x);
+            }
+            TextView t = findViewById(R.id.textResultado);
+            t.setText("Resultado: "+l.length());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void ir(View v) {
+        TextView id_heroe = findViewById(R.id.id_heroe);
+        String buscar = id_heroe.getText().toString();
+        System.out.println(buscar);
+        try {
+            JSONArray l = (JSONArray) busqueda.get("results");
+            for (int i  = 0; i < l.length(); i++) {
+                JSONObject j = (JSONObject) l.get(i);
+                System.out.println(j.get("id"));
+                if (j.get("id").toString() == buscar.toString()) {
+                    Intent intent = new Intent(this, GraficoBarras.class);
+                    startActivity(intent);
+                }
+            }
+        } catch (JSONException e) {
+            System.out.println(e);
+        }
     }
 }
